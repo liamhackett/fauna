5d8fd6f6c3bdbb155ff1375b4a65dea32a1b95a0
Liam Hackett <ljh1oo@yahoo.com>
2023-09-25 15:33:19 -0400

    Converted to html

--- connect-DB.php
+++
@@ -1,8 +0,0 @@
-<?php
-$databaseName = 'WHACKETT_lab';
-$dsn = 'mysql:host=webdb.uvm.edu;dbname=' . $databaseName;
-$username = 'whackett_writer';
-$password = 'DsLxFUKeno51';
-
-$pdo = new PDO($dsn, $username, $password);
-?>
